eb79f743096c5fca15926d784d32dc483458417b
Italo Xavier <xavieritalo853@gmail.com>
2024-06-26 16:17:55 -0300

    Add files via upload

---
+++ página_de_login/bd/localhost.sql
@@ -0,0 +1,42 @@
+# MySQL-Front 5.1  (Build 4.13)
+
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
+/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
+/*!40103 SET SQL_NOTES='ON' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+
+# Host: localhost    Database: usuarios
+# ------------------------------------------------------
+# Server version 5.5.25a
+
+#
+# Source for table perfil
+#
+
+DROP TABLE IF EXISTS `perfil`;
+CREATE TABLE `perfil` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Dumping data for table perfil
+#
+
+LOCK TABLES `perfil` WRITE;
+/*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
+/*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
---
+++ página_de_login/php/verificacao.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require 'connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT id, email, password FROM perfil WHERE email = ?";
+    $stmt = $con->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+
+        if ($user && password_verify($senha, $user['password'])) {
+            // Se encontrou um usuário com essas credenciais, redireciona para a página inicial
+            $_SESSION['user_id'] = $user['id']; // Assumindo que 'id' é a chave primária do usuário
+            header("Location: ../index.php");
+            exit();
+        } else {
+            // Se não encontrou, pode redirecionar de volta para a página de login ou mostrar uma mensagem de erro
+            echo "Credenciais inválidas. Por favor, tente novamente.";
+        }
+
+        $stmt->close();
+    } else {
+        echo "Erro na preparação da consulta: " . $con->error;
+    }
+}
+?>
